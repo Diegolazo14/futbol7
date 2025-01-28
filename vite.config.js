4d7ac15d3dd755156ca38e87e3ed75e6a12fb594
Diegolazo <diego.astorgamunoz@gmail.com>
2025-01-28 00:52:15 -0300

    Actualizar configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,18 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: ".", // Carpeta raíz del proyecto
+  base: "/", // Configuración de la ruta base para despliegue
   server: {
     port: 5173, // Puerto por defecto de Vite
+  },
+  build: {
+    outDir: "dist", // Directorio donde se guardará la compilación
+    rollupOptions: {
+      input: {
+        main: "./public/index.html", // Ruta al archivo HTML principal
+        match: "./public/match.html", // Ruta a otros archivos HTML
+        profile: "./public/profile.html", // Ruta a otros archivos HTML
+      },
+    },
   },
 });
