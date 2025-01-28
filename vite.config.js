b47356060585034794992900209d971983990e6b
Diegolazo <diego.astorgamunoz@gmail.com>
2025-01-28 00:39:16 -0300

    Proyecto funcional con Firebase y cronómetros

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: ".", // Carpeta raíz del proyecto
+  server: {
+    port: 5173, // Puerto por defecto de Vite
+  },
+});
